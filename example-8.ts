5147f559fc33117006d42062a302b2d46e23add7
Mitchell <soggybag@gmail.com>
2022-12-05 09:20:38 -0800

    updated

--- example-8.ts
+++ example-8.ts
@@ -1,6 +1,11 @@
 // We need a Kaiju class
 // All Kaiju have name, power and type. 
+// Kaiju should have a describe method that 
+// prints: name:<name> type:<type> power:<power>
+
 // Type can be: lizard, flying, or ape. 
+// Use an enum to define the type
+
 // Be sure to include a constructor in the 
 // Kaiju class that initializes it's properties!
 
