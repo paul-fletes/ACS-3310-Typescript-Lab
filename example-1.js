6cf706d807d0b91b81f8989145eddccc829c1f2b
Paul Fletes <paul.fletes@gmail.com>
2023-09-13 12:49:10 -0500

    complete ex 1

--- example-1.js
+++ example-1.js
@@ -13,4 +13,18 @@ console.log(answer)
 // Does it look right? 
 // What went wrong?
 
+// .toFixed() method converts the 'amount' value to a string with
+// 2 decimal places. This causes the 'tax' vaule to be the result 
+// of multiplying a string with a number. This then affects the 
+// return value of getPriceWithTax(), because you are effectively
+// concatenating a string (price) with a number (tax). To fix this, 
+// append the .toFixed() method to the return value of (price + tax)
 
+function correctPriceWithTax(amount, rate) {
+	const price = amount
+	const tax = price * (rate / 100)
+	return (price + tax).toFixed(2)
+}
+
+const correctAmount = correctPriceWithTax(23.99, 9.5)
+console.log(`The correct amount is: ${correctAmount}`)
