920e7eae1ce6aa050e12fde7d97525d889be0665
Paul Fletes <paul.fletes@gmail.com>
2023-09-13 15:55:04 -0500

    complete ex 12

--- example-12.ts
+++ example-12.ts
@@ -6,27 +6,33 @@
 // Use a string enum: https://www.typescriptlang.org/docs/handbook/enums.html#string-enums
 
 enum Direction {
-	
+	North = "NORTH",
+	South = "SOUTH",
+	East = "EAST",
+	West = "West"
 }
 
 // Should having a heading property type Direction
 
 class MapPosition {
+	heading: Direction
 
-	constructor() {
-		
+	constructor(heading: Direction = Direction.East) {
+		this.heading = heading
 	}
 
 	// takes a new Direction as an argument
 	move(newDirection: Direction) {
 		// set the direction on your property 
 		// Print the new direction
+		this.heading = newDirection;
+		console.log(`New direction - You are heading ${this.heading}`)
 	}
 
 	// Should return a string and print:
 	// "you are heading <direction>"
 	describe(): string {
-		return ''
+		return `You are heading ${this.heading}`;
 	}
 }
 
@@ -34,8 +40,8 @@ const location = new MapPosition()
 console.log(location.describe())
 // Might output:
 // "You are heading north"
-location.move(/* add a direction here */)
-
+location.move(Direction.North)
+console.log(location.describe())
 
 
 export default MapPosition
