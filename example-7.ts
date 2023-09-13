5b4cdd51f0f50221efe560e8a8a25aa606bb496b
Paul Fletes <paul.fletes@gmail.com>
2023-09-13 13:42:15 -0500

    complete ex 7

--- example-7.ts
+++ example-7.ts
@@ -8,14 +8,20 @@
 // https://www.typescriptlang.org/docs/handbook/enums.html#handbook-content
 
 enum Suit {
-
+	Hearts,
+	Diamonds,
+	Clubs,
+	Spades
 }
 
 type Card = { suit: Suit, value: number }
 
 const ace: Card = {
-
+	suit: Suit.Spades,
+	value: 1
 }
+
+console.log(ace);
 
 // Compile the code and check what type script says: 
 // tsc example-7.ts
